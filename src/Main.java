5d42cadb1cb983eacb42c29e4b43046aad3525d2
grobulus1 <105735685+grobulus1@users.noreply.github.com>
2024-03-19 10:30:44 +0100

    g

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
-public class Main {
+public class Main{
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Point p1 = new Point(100, 10);
+        Point p2 = new Point(10, 100);
+        Segment segment = new Segment(p1, p2);
+        System.out.println(segment.length());
     }
 }
