23671a41f45271f2a639354c0a7933b358ff79e3
grobulus1 <105735685+grobulus1@users.noreply.github.com>
2024-03-22 11:54:31 +0100

    change Polygon

--- src/Polygon.java
+++ src/Polygon.java
@@ -4,6 +4,14 @@ public class Polygon {
     public Polygon(Point[] points) {
         this.points = points;
     }
+    public String toSvg(){
+        StringBuilder poly = new StringBuilder();
+        poly.append("<polygon points=\"");
+        for(Point point: points){
+            poly.append(point.x).append(",").append(point.y).append(" ");
+        }
+        poly.append("\" style=\"fill:none;stroke:black;stroke-width:1\"/>\n");
+        return poly.toString();
+    }
 
 }
-
